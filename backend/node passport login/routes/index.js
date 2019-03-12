2a37df5a48b33fbaa885884181673bf1f86aad5f
fifthspoon <fifthspoon@gmail.com>
2019-03-12 10:50:38 +0100

    moved files to local to prevent issues while testing

--- backend/node passport login/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// welcome page, render welcome.ejs
-router.get('/', (require, respond) => response.render('Welcome'))
-
-
-module.exports = router
