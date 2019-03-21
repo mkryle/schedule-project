a2fc3bbeccd005c5b11140724c5f41232c7f52ae
fifthspoon <fifthspoon@gmail.com>
2019-03-21 09:39:08 +0100

    added startpage to router

--- js-files/router.js
+++ js-files/router.js
@@ -1,20 +1,22 @@
-import Router  from 'vue-router'
+import Router from 'vue-router'
 
 import loginPage from './components/signInPage.vue'
 import adminRouter from './router-admin'
 import UserRouter from './router-user'
+import startPage from './components/startpage.vue'
 
-const baseRoutes = [
-  {
+const baseRoutes = [{
     path: '/login',
     name: 'login',
     title: 'login-Page',
-    component: loginPage,
+    component: startPage,
   },
   {
     path: '*',
-    redirect: { name: 'login' },
-},
+    redirect: {
+      name: 'login'
+    },
+  },
 ];
 
 const routes = baseRoutes.concat(adminRouter, UserRouter);
