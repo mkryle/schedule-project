3397a89f17a981260eabbe1c62c934eddf34fa33
Marcus Kryle <marcus.kryle@gmail.com>
2019-03-28 13:57:01 +0100

    fix get funct from db

--- backend/index.js
+++ backend/index.js
@@ -13,22 +13,15 @@ app.use(function (request, result, next) {
     result.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
-
 //skapa databas
 let database
-
-
 //assign databas
 sqlite.open('databases/schema.sqlite').then(database_ => {
     database = database_
 }) // Open DB end 
 
-app.use(function (request, result, next) {
-    result.header('Access-Control-Allow-Origin', '*');
-    result.header('Access-Control-Allow-Headers', 'Content-Type');
-    result.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    next();
-});
+
+
 //get all
 app.get('/', (request, response) => {
     database.all('SELECT * FROM schema').then(schema => {
@@ -37,37 +30,27 @@ app.get('/', (request, response) => {
 }) // get ends
 
 
-// get datum
-app.get('/bydate/:datum', (request, response) => {
-    database.run('SELECT * FROM schema WHERE date=?;',
-        [request.params.date]
-    ).then(schema => {
-        response.send(schema)
-    }) // db all end
-    response.status(201)
-}) //db run close
-
-// get topName
-app.get('/bytopname/:topName', (request, response) => {
-    database.run('SELECT * FROM schema WHERE topName=?;',
-        [request.params.topName]
-    ).then(schema => {
-        response.send(schema)
-    }) // db all end
-    response.status(201)
-}) //db run close
-
-
-
 // get createdBy
-app.get('/createdby/:namn', (request, response) => {
-    database.run('SELECT * FROM schema WHERE createdBy=?;',
+app.get('/createdBy/:namn', (request, response) => {
+    database.all('SELECT * FROM schema WHERE createdBy=?;',
         [request.params.namn]
     ).then(schema => {
         response.send(schema)
     }) // db all end
     response.status(201)
 }) //db run close
+
+
+// get datum
+app.get('/bydate/:datum', (request, response) => {
+    database.all('SELECT * FROM schema WHERE date=?;',
+        [request.params.datum]
+    ).then(schema => {
+        response.send(schema)
+    }) // db all end
+    response.status(201)
+}) //db run close
+
 
 
 //post för att lägga till i schemat
@@ -97,16 +80,5 @@ app.delete('/:deleteTask', (request, response) => {
 
 
 
-// put för att ända i schemat via id
-app.put('/:id', (request, response) => {
-    database.run('UPDATE schema SET date=?, startTime=?, endTime=?, topName=?, subName=?, altSubName=?, eventName=?, createdBy=?, priavateLink=?, WHERE id=?;',
-        [request.body.date, request.body.startTime, request.body.endTime, request.body.topName, request.body.subName, request.body.altSubName, request.body.eventName, request.body.createdBy, request.body.privateLink, request.params.id]
-    ).then(() => {
-        database.all('SELECT * FROM schema').then(schema => {
-            response.send(schema)
-        }) // db all end
-        response.status(201)
-    }) //db run close
-}) // app del close
 
 app.listen(3000)
