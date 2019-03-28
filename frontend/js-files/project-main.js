4577ed1613fe7edc09dc63b04fde72113cd1db26
ziampas <kristian.olausson@gmail.com>
2019-03-28 11:52:09 +0100

    Move frontend files to frontend directory

--- frontend/js-files/project-main.js
+++ frontend/js-files/project-main.js
@@ -7,7 +7,7 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router'
 import store from './store'
-import moment from './moment'
+// import moment from 'moment'
 // import FullCalendar from 'vue-full-calendar'
 import 'bulma/css/bulma.css'
 
