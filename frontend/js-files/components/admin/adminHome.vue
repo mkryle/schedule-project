84e7f41cc3e48fb1bee749e0beaa76915cc35c80
Jan <grg_jan@hotmail.com>
2019-04-01 19:39:00 +0200

    this is the landing page for Admin

--- frontend/js-files/components/admin/adminHome.vue
+++ frontend/js-files/components/admin/adminHome.vue
@@ -1,30 +1,14 @@
 <template>
     <div>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <div class="navbar-item">
-        <h2>{{ msg }} {{ $route.params.id }}</h2>
-      </div>
+
+    <div class="notification is-danger">
+        welcome {{$route.params.id}} to home page
     </div>
-    <div class="navbar-menu">
-      <div class="navbar-start">
-        <p class="navbar-item"><router-link to="/admin/adminName/">Home</router-link></p>
-        <p class="navbar-item"><router-link to="/admin/adminName/events">Events</router-link></p>
-        <p class="navbar-item"><router-link to="/admin/adminName/profile">Profile</router-link></p>
-        <p class="navbar-item"><router-link to="/admin/adminName/students">Students</router-link></p>
-      </div>
-    </div>
-  </nav>
-      <router-view></router-view>
 </div>
 
 </template>
 <script>
     export default{
-        data(){
-          return{
-            msg: 'Im logged as: '
-          }
-        }
+
     }
 </script>
