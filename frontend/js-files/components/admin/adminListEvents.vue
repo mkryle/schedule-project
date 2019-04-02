0e1d2fa9cd6f12055a1cc78078cc75952b69de0d
Mikael <48313189+fifthspoon@users.noreply.github.com>
2019-04-02 11:36:39 +0200

    added addNewEvent

--- frontend/js-files/components/admin/adminListEvents.vue
+++ frontend/js-files/components/admin/adminListEvents.vue
@@ -1,5 +1,27 @@
 <template>
     <div>
+    
+            <div>
+            <label for="date">Datum:</label>
+            <input type="date" value="date" v-model="date">
+            <label for="startTime">Start tid:</label>
+            <input type="time" name="startTime" v-model="startTime">
+            <label for="endTime">Slut tid:</label>
+            <input type="time" name="endTime" v-model="endTime">
+            <label for="topName">Skola / Arbete:</label>
+            <input type="text" name="topName" v-model="topName">
+            <label for="subName">Linje / Avdelning:</label>
+            <input type="text" name="subName" v-model="subName">
+            <label for="altSubName">Klass / Team:</label>
+            <input type="text" name="altSubName" v-model="altSubName">
+            <label for="eventName">Lektiontyp / Event:</label>
+            <input type="text" name="eventName" v-model="eventName">
+            <label for="createdBy">Skapad av: (namn)</label>
+            <input type="text" name="createdBy" v-model="createdBy">
+
+            <input type="button" id="add-to-schedule" value="send" v-on:click="addToSchedule()">
+
+        </div>
 
     <div id="week">
       <div class="day">
